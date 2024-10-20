c37f049aed729e68bc3f0d57df6474ae4169a16e
Noah Tah <noahtah21@gmail.com>
2024-10-20 16:20:13 -0500

    completed a large chunk and added a flowchart

--- WindowsClient.cpp
+++ WindowsClient.cpp
@@ -5,6 +5,7 @@
 
 #pragma comment(lib, "Ws2_32.lib") // tells linker this library file is needed
 #define DEFAULT_PORT "8080"
+
 // Client Code Main Functionality
 //
 // 1. Initialize Winsock
@@ -51,7 +52,10 @@ int main() {
     hints.ai_protocol = IPPROTO_TCP;
 
     // Resolve the server address and port
-    iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+    
+    // iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+    iResult = getaddrinfo("localhost", DEFAULT_PORT, &hints, &result);
+
     if (iResult != 0) {
         std::cout << "getaddrinfo failed, returned: " << iResult << std::endl;
         WSACleanup();
@@ -79,7 +83,74 @@ int main() {
         WSACleanup();
         return 1;
     }
+
+    //----------------------//
+    // Connect to the Server
+    //----------------------//
     
+    iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        closesocket(ConnectSocket);
+        ConnectSocket = INVALID_SOCKET;
+    }
+
+    // Should really try the next address returned by getaddrinfo
+    // if the call connect failed
+    // But for now we just free the resources 
+    // returned by getaddrinfo and print the error message
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        std::cout << "INVALID_SOCKET! Unable to connect to server!" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+    
+    //----------------------//
+    // Send and Receive Data
+    //----------------------//
+
+    #define DEFAULT_BUFLEN 512
+
+    int recvbuflen = DEFAULT_BUFLEN;
+
+
+    const char *sendbuf = "this is a test";
+    char recvbuf[DEFAULT_BUFLEN];
+
+    // Send an initial buffer
+    iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+    if (iResult == SOCKET_ERROR ) {
+        std::cout << "send failed, returned: " << WSAGetLastError() << std::endl;
+        closesocket(ConnectSocket);
+        return 1;
+    }
+
+    std::cout << "Bytes Sent: " << iResult << std::endl;
+
+    // shutdown the connection for sending since no more data will be sent
+    // the client can still use the ConnectSocket for receiving data
+    iResult = shutdown(ConnectSocket, SD_SEND);
+    if(iResult == SOCKET_ERROR) {
+        std::cout << "shutdown failed, returned: " << WSAGetLastError() << std::endl;
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Receive data until the server closes the connection
+
+    do {
+        iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+        if (iResult > 0) {
+            std::cout << "Bytes received: " << iResult << std::endl;
+        } else if (iResult == 0 ) {
+            std::cout << "Connection closed" << std::endl;
+        } else {
+            std::cout << "recv failed, returned: " << WSAGetLastError() << std::endl;
+        } 
+    } while (iResult > 0);
 
 
 
