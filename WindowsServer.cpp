723d67466c22410640268dee2c3472092c4b8ec1
noah-tah <noahtah21@gmail.com>
2024-10-27 22:14:22 -0500

    cleaned up the code significantly, now to continue progressing

--- WindowsServer.cpp
+++ WindowsServer.cpp
@@ -3,9 +3,11 @@
 // Northern Oklahoma College
 // C++ Progamming
 // ---------------------------//
+
+// ---------------------------//
 // Libraries & Setup
-//
-//
+// ---------------------------//
+
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
@@ -14,14 +16,17 @@
 #include <thread>
 #include <atomic>
 #include <string>
-//
-#pragma comment(lib, "Ws2_32.lib") // tells linker this library file is needed
-//
+
+// Link with Ws2_32.lib (Windows Sockets Library)
+#pragma comment(lib, "Ws2_32.lib") 
+
+//----------------------//
 // End Libraries & setup
-// ---------------------- //
-//
+//----------------------//
+
+//----------------------//
 // Global Definitions
-// ---------------------- //
+//----------------------//
 
 #define DEFAULT_PORT "8080"
 WSADATA wsaData;
@@ -46,42 +51,33 @@ SOCKET acceptConnection();
 int handleRequests(SOCKET ClientSocket);
 
 
-//
-//
+// -------------------------------//
 // End Global Definitions
 // -------------------------------//
 
 
 
+//----------------------//
+//Function Definitions
+//----------------------//
 
-//
-// Function Definitions
-//
 
-
-//
-// Function setting the addressinfo struct to the desired values
-//
+/*
+Function setting the addressinfo struct to the desired values
+Zero out the memory of the hints, which is a pointer to the adddrinfo struct
+Set the address family to a family containg IPv4, and set socket type to stream socket (TCP)
+Set the socket type to stream socket (TCP)
+Set the protocol to TCP
+AI_PASSIVE means the socket address will be called to bind()
+*/
 
 int configureSocketHints(struct addrinfo* hints) {
-
-    // Zero out the memory of the vairable hints, which is a pointer to the addrinfo struct
     ZeroMemory(hints, sizeof(*hints)); 
-
-    // Set the address family to IPv4
     hints->ai_family = AF_INET;
-
-    // Set the socket type to stream socket (TCP)
     hints->ai_socktype = SOCK_STREAM;
-
-    // Set the protocol to TCP
     hints->ai_protocol = IPPROTO_TCP;
-
-    // The socket address will be used in the call to bind()
     hints->ai_flags = AI_PASSIVE;
-
-
-   return 0; 
+    return 0; 
 }
 
 void printSocketCreatedSuccess() {
@@ -91,25 +87,19 @@ void printSocketCreatedSuccess() {
     std::cout << "Socket bound with ai_protocol: " << result->ai_protocol << std::endl;
 }
 
-
+/*
+Set up address info struct to store address information
+Resolve the local address and port to be used by the server
+- We use NULL for the node parameter to indicate that we want to use a local address
+- We use DEFAULT_PORT for the service parameter to specify the port number
+- We use the hints struct to specify the desired properties of the socket
+- The result parameter will point to a linked list of addrinfo structures containing the resolved address
+iResult just contains the integer status code
+*/
 
 int initializeSocketPresets() {
-
-    // Set up the address info struct to store the address information
     configureSocketHints(&hints);
-
-    //
-    // Resolve the local address and port to be used by the server
-    // - We use NULL for the node paramter to indicate that we want to use a local address 
-    // - We use DEFAULT_PORT for the service paramter to specify the port number
-    // - We use the hints struct to specify the desired properties of the socket 
-    // - The result parameter will point to a linked list of addrinfo structures containing the resolved address 
-    //
-    // iResult just contains the integer status code
-    //
-
     iResult = getaddrinfo(NULL,DEFAULT_PORT,&hints,&result);
-
     if (iResult != 0) {
         std::cout << "getaddrinfo failed " << iResult << std::endl;
         WSACleanup();
@@ -134,22 +124,23 @@ int extractIPv4() {
     return 0;
 }
 
+/*
+Create a socket using the address information from getaddrinfo which was stored in the result variable
+This is possible because the result variable points to the linked list of addrinfo structures
+containing the resolved addresses
+*/
+
 int createSocket() {
-    // Create a socket using the address information from getaddrinfo which was stored in the result variable
-    // This is possible because the result variable points to the linked list of addrinfo structures containing the resolved address
     ListenSocket = socket(result->ai_family,result->ai_socktype,result->ai_protocol);
     std::cout << "Initialied ListenSocket!" << std::endl;
-
-    // Check if the socket creation failed
     if(ListenSocket == INVALID_SOCKET) {
         std::cout << "Error at socket(): " << WSAGetLastError() << std::endl;
-        freeaddrinfo(result);  // Free the address information
-        WSACleanup(); // Clean up Winsock
+        freeaddrinfo(result);
+        WSACleanup();
         return 1;
     } else {
-        printSocketCreatedSuccess(); // Print success message
+        printSocketCreatedSuccess(); 
     }
-
     return 0;
 }
 
@@ -158,36 +149,28 @@ void welcomeToProgram () {
     std::cout << "Initializing Winsock... Running WSAStartup" << std::endl;
 }
 
-//
-// Run WSAstartup to initialize Winsock
-//
+/*
+Run WSAstartup to initialize Winsock
+*/
 
 int startWSA() {
     welcomeToProgram();
-
-
     iResult = WSAStartup(MAKEWORD(2,2),&wsaData);    
-
     if (iResult != 0) {
         std::cout << "WSAStartup failed, returned: "<< iResult << std::endl;
         return 1;
     } else {
         std::cout << "WinSock Description: " << wsaData.szDescription << std::endl;
     }
-
     return 0;
 
 }
 
-//
-// Bind the socket to the address and port specified in the addrinfo data structure
-//
+/*
+Bind the socket to the address and port specified in the addrinfo data structure
+*/ 
 
 int bindSocket() {
-
-    // Setup the TCP listening socket
-
-
     int iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
     if (iResult == SOCKET_ERROR) {
         std::cout << "Bind failed with error: " << WSAGetLastError() << std::endl;
@@ -202,9 +185,9 @@ int bindSocket() {
     return 0;
 }
 
-//
-// Listen on the socket, taking in the socket and the backlog which is the maximum number of connections that can be queued 
-//
+/*
+Listen on the socket, taking in the socket and the backlog which is the maximum number of connections that can be queued 
+*/
 
 int listenOnSocket() {
     if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
@@ -218,26 +201,23 @@ int listenOnSocket() {
     return 0;
 }
 
-//
-// Accept Connections
-//
+/*
+Accept Connections
+create sockaddr_in data strcuture to store client information
+call accept() to accept the connection
+    s: socket we are listening on
+    addr: pointer to a sockaddr structure that will be filled with the address of the client that is connecting
+    addrlen: pointer to an int that will be filled with the size of the address of the client that is connecting
+if accept() returns a valid socket, then we have a connection
+    print the IP address of the client
+    return the client socket        
+*/
+
 
 SOCKET acceptConnection() {
-
-
     SOCKET ClientSocket = INVALID_SOCKET;
-
-    // Data structure to store client information
     struct sockaddr_in clientInfo;
-
-    // This the size of sockaddr_in struct we use to store client information
     int clientInfoSize = sizeof(clientInfo);
-
-    //
-    // The addr parameter in the accept() function can point to a sockaddr structure
-    // That will be filled with the address of the client that is connecting
-    //
-
     ClientSocket = accept(ListenSocket, (struct sockaddr*)&clientInfo, &clientInfoSize); 
     if (ClientSocket == INVALID_SOCKET) {
         std::cout << "Failed to accept socket! " << WSAGetLastError() << std::endl;
@@ -245,32 +225,29 @@ SOCKET acceptConnection() {
         WSACleanup();
         return INVALID_SOCKET;
     } else {
-
-        // Extract the IPv4 address of the client
         struct sockaddr_in *ipv4 = (struct sockaddr_in *)&clientInfo;
-        
         char ipstr[INET_ADDRSTRLEN];
-
         inet_ntop(AF_INET, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
-
         std::cout << "Client connected with IP Address: " << ipstr << std::endl;
     }
-
     return ClientSocket;
-
 }
+
+/*
+used recv() to receive data from the client
+if recv() returns a positive value, then we have received data
+    print the number of bytes received
+    process the received data
+    send an HTTP response
+*/
 
 int handleRequests(SOCKET ClientSocket) {
     char recvbuf[512];
     int iResult;
     int recvbuflen = 512;
-
     iResult = recv(ClientSocket, recvbuf, recvbuflen, 0); 
     if (iResult > 0) {
         std::cout << "Bytes received: " << iResult << std::endl;
-
-        // Process the received data (for simplicity, we assume it's an HTTP GET request)
-        // Send an HTTP rersponse
         const char* httpResponse = "HTTP/1.1 200 OK\rr\nContent-Type: text/html\r\\n\r\n<html><body><h1>Hello, World!</h1></body></html>";
         send(ClientSocket, httpResponse, strlen(httpResponse), 0);
     } else if (iResult == 0) {
@@ -278,17 +255,27 @@ int handleRequests(SOCKET ClientSocket) {
     } else {
         std::cout << "recv failed with error: " << WSAGetLastError() << std::endl;
     }
-
-
-    // Close the client socket
     closesocket(ClientSocket);
     return 0;
 }
+
+/*
+This cleanup function closes the socket,
+then calls WSACleanup to clean up Winsock which will unload the Winsock 2.2 DLL which will free the memory allocated by WSAStartup
+*/
 
 void cleanup() {
     closesocket(ListenSocket);
     WSACleanup();
 }
+
+/*
+This loop function will run as long as the running flag is set to true which is an atomic boolean
+which is set to true by default which means the server will run until the running flag is set to false
+which will happen when the server is shutting down which will be done by the main function which will set the running flag to false
+which will cause the server loop to break out of the loop and the server will shut down which will call the cleanup function
+which will close the socket and clean up Winsock
+*/
 
 void serverLoop() {
     while (running) {
@@ -298,52 +285,35 @@ void serverLoop() {
     }
 }
 
-//
+
+//----------------------//
 // End Function Definitions
-//
+//----------------------//
 
 
-
+//----------------------//
 // Main Server Code
 //----------------------//
 
-// # Functions of our Windows Server
-
-// 1. Initialize WSA - WSAStartup()
-// 2. Create a socket - socket()
-// 3. Bind the socket - bind()
-// 4. Listen on the socket - listen()
-// 5. Accept a connection - accept(), connect()
-// 6. Send and receive data - recv(), send(), recvfrom(), sendto()
-// 7. Disconnect - closesocket()
-
+/*
+Functions of our Windows Server
+    1. Initialize WSA - WSAStartup()
+    2. Create a socket - socket()
+    3. Bind the socket - bind()
+    4. Listen on the socket - listen()
+    5. Accept a connection - accept(), connect()
+    6. Send and receive data - recv(), send(), recvfrom(), sendto()
+    7. Disconnect - closesocket()
+*/
 
 int main () {
-
     if (startWSA() != 0) return 1; 
     if (initializeSocketPresets() != 0) return 1;
     if (createSocket() != 0) return 1; 
     if (extractIPv4() != 0) return 1;
     if (bindSocket() != 0) return 1;
     if (listenOnSocket() != 0) return 1;
-
-    // std::thread serverThread(serverLoop);
-
-
-    // closesocket(ListenSocket);
-    // serverThread.join();
-    // cleanup();
-
-
-    // while (true) {
-    //     SOCKET ClientSocket = acceptConnection();
-    //     if (ClientSocket == INVALID_SOCKET) break;
-    //     handleRequests(ClientSocket);
-    // }
-
     SOCKET ClientSocket = INVALID_SOCKET;
-
-    // Accept single client socket
     ClientSocket = accept(ListenSocket, NULL, NULL);
     if (ClientSocket == INVALID_SOCKET) {
         std::cout << "Accept failed: " << WSAGetLastError() << std::endl;
@@ -351,10 +321,6 @@ int main () {
         WSACleanup();
         return 1;
     }
-
-
-
-
     WSACleanup();
     return 0;
 }
